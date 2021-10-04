be8040555be875121d226fb213de4831f8190ac5
Huy Hua <huyhua@nvg.vn>
2021-10-04 14:57:01 +0700

    add test data

--- cypress/integration/WeatherDisplay.spec.js
+++ cypress/integration/WeatherDisplay.spec.js
@@ -2,38 +2,25 @@ import dayjs from 'dayjs'
 import { homePage } from '../pages/Home'
 import { resultPage } from '../pages/Result'
 import { findPage } from '../pages/Find'
+import { cities } from '../fixtures/test_data/city.json'
 
-const today = dayjs().format('MMM DD')
+const today = dayjs().format('MMM D')
 
-describe('Open Weather Map', () => {
+describe('Verify city temperature', () => {
   beforeEach(() => {
     homePage.visit()
   })
 
-  it('from body', () => {
-    // Stub find requests to increase test speed
-    cy.intercept(/\/data\/2.5\/find\?q=Hanoi.*/, {
-      fixture: 'find.json',
+  cities.forEach(([keyword, result]) => {
+    it(`with keyword ${keyword}`, () => {
+      homePage.searchFromHeader(keyword)
+      findPage.selectLocation(0)
+      resultPage.city.should('have.text', result)
+      resultPage.temperature.invoke('text').should('match', /\d{2}°(C|F)/)
+      // Hour and minute matching is flaky and should only be style matched
+      resultPage.date
+        .invoke('text')
+        .should('match', new RegExp(`^${today}, \\d{2}:\\d{2}(am|pm)`))
     })
-
-    homePage.search('Hanoi').selectDropdown(0)
-
-    resultPage.city.should('have.text', 'Ha Noi, VN')
-    resultPage.temperature.invoke('text').should('match', /\d{2}°C/)
-    // Hour and minute matching is flaky and should only be style matched
-    resultPage.date
-      .invoke('text')
-      .should('match', new RegExp(`^${today}, \\d{2}:\\d{2}(am|pm)`))
-  })
-
-  it('from header', () => {
-    homePage.searchFromHeader('Hanoi')
-    findPage.selectLocation(0)
-    resultPage.city.should('have.text', 'Hanoi, VN')
-    resultPage.temperature.invoke('text').should('match', /\d{2}°C/)
-    // Hour and minute matching is flaky and should only be style matched
-    resultPage.date
-      .invoke('text')
-      .should('match', new RegExp(`^${today}, \\d{2}:\\d{2}(am|pm)`))
   })
 })
